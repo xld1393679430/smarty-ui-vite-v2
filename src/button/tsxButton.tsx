2c25cc69a893feb6ef580628ccd1f255f0a83a67
ld_xu <ld_xu@trip.com>
2022-08-26 14:39:36 +0800

    :sparkles: 引入vue & vue plugin

---
+++ src/button/tsxButton.tsx
@@ -0,0 +1,9 @@
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'tsxButton',
+    render() {
+        // return 1
+        return <button>button--tsx</button>
+    }
+})
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+
+import SButton from './button/index'
+import SFCButton from './button/button.vue'
+import TsxButton from './button/tsxButton'
+
+createApp(TsxButton).mount('#root')
